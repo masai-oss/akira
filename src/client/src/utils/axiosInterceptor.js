4e3837f577d71c4008d4ed1f361dac208226b864
KarthikeyanRanasthala <karthikeyan.ranasthala@gmail.com>
2020-01-14 20:05:18 +0530

    Fix Interceptor

--- src/client/src/utils/axiosInterceptor.js
+++ src/client/src/utils/axiosInterceptor.js
@@ -15,6 +15,10 @@ axiosInstance.interceptors.request.use(request => {
   if (interceptorEnabled) {
     console.log(`Request: ${request.method} ${request.baseURL}${request.url}`);
   }
+  const token = localStorage.getItem("akira-auth-token");
+  if (token !== null) {
+    request.headers = { token };
+  }
   return request;
 });
 
@@ -22,9 +26,10 @@ axiosInstance.interceptors.response.use(
   response => {
     if (interceptorEnabled) {
       console.log("Response Status: ", response.status);
-      console.log("Response Summary: ", response.data.summary);
+      console.log("Response Comment: ", response.data.comment);
       console.log("Response Content: ", response.data.data);
     }
+    return response.data.data;
   },
   error => console.log(error)
 );
